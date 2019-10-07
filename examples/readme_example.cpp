4988a9019e54eefc58236c937c450b1d45ca6552
Jan Lafferton <jan.lafferton@gmx.de>
2019-10-07 11:48:27 +0200

    Add readme example

--- examples/readme_example.cpp
+++ examples/readme_example.cpp
@@ -1,11 +1,3 @@
-# libst library
-C++17 header only compile time state machine implementation
-
-* Zero dependencies (Only STL includes)
-* Compiles fast
-
-## Example usage
-```cpp
 #include <libst.hpp>
 #include <iostream>
 
@@ -39,19 +31,3 @@ int main(int argc, char** argv) {
         stateMachine.update();
     }
 }
-```
-
-Output
-```
-Data is 1
-Data is 2
-Data is 3
-Data is 4
-Data is 5
-Data is 6
-Data is 7
-Data is 8
-Data is 9
-Data is 10
-Calculation finished
-```
